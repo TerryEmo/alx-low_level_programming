228ad2fa19776606d5098987e7b1b93a32dfc901
TerryEmo <terrance.sabelo@gmail.com>
2024-01-18 07:18:15 -0800

    0x1E. C - Search Algorithms

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,48 +1,37 @@
 #include "search_algos.h"
 
 /**
-  * jump_search - search for the value in an array integer by the use of linear search
-  * @array: pointer to the first element of the array to search
-  * @size: number of elements in the array
-  * @value: value to search for
-  * Return: If value is not present or array is NULL, -1
-  * Otherwise, first index where the value is located
-  */
+ * jump_search - function that searches for a value
+ * in a sorted array of integers
+ *
+ * @array: a pointer to the first element of the array to search in
+ * @size:  the number of elements in array
+ * @value: is the value to search for
+ * Return: the first index where value is located
+ */
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, jump, step;
+	size_t i, step, jump;
 
-	/* Check if the array is NULL or empty */
 	if (array == NULL || size == 0)
 		return (-1);
 
-	/* Calculate the jump step size */
 	step = sqrt(size);
 
-	/* Perform the jump search */
 	for (i = jump = 0; jump < size && array[jump] < value;)
 	{
-		printf("Value checked array[%ld] = [%d]\n", jump, array[jump]);
-
-		/* Store the previous jump position */
+		printf("Value checked array [%ld] = [%d]\n", jump, array[jump]);
 		i = jump;
-
-		/* Update the jump position */
 		jump += step;
 	}
 
 	printf("Value found between indexes [%ld] and [%ld]\n", i, jump);
 
-	/* Adjust the jump position */
-	jump = jump < size - 1 ? jump : size - 1;
-
-	/* Perform a linear search within the identified range */
+	jump = jump > size - 1 ? jump : size - 1;
 	for (; i < jump && array[i] < value; i++)
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		printf("Value checked array [%ld] = [%d]\n", i, array[i]);
+	printf("Value checked array [%ld] = [%d]\n", i, array[i]);
 
-	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
-	/* Check if the value is found and return the corresponding index */
 	return (array[i] == value ? (int)i : -1);
 }
